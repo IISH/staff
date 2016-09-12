0d3b216c5ef84d62979576969db4429d61364d6e
Gordan Cupac <gcu@iisg.nl>
2016-09-12 11:17:43 +0200

    fixed visibility fire link

--- print.php
+++ print.php
@@ -20,13 +20,22 @@ echo $oPage->getPage();
 $dbConn->close();
 
 function createSpecialNumbersContent( ) {
+	global $oWebuser;
+
 	$ret = "
 <h1>" . Translations::get('header_print') . "</h1>
 <i><font size=-1>(" . Translations::get('sorted_on') . ":)</font></i><br>
 <ol>
 	<li><a href=\"print_firstname.php\" target=\"_blank\">" . Translations::get('lbl_firstname') . " <img src=\"images/misc/popup.png\"></a></li>
-	<li><a href=\"print_lastname.php\" target=\"_blank\">" . Translations::get('lbl_lastname') . " <img src=\"images/misc/popup.png\"></a><br><br>" . Translations::get('or_go_to') . "<br><br></li>
-	<li><a href=\"fire.php\">" . strtoupper(Translations::get('menu_fire')) . "</a><br><br></li>
+	<li><a href=\"print_lastname.php\" target=\"_blank\">" . Translations::get('lbl_lastname') . " <img src=\"images/misc/popup.png\"></a>";
+
+	// show fire link only when allowed to see the fire tab
+	if ( $_SESSION["FIRE_KEY_CORRECT"] == '1' || $oWebuser->hasAuthorisationTabFire() ) {
+		$ret .= "<br><br>" . Translations::get('or_go_to') . "<br><br></li>
+			<li><a href=\"fire.php\">" . strtoupper(Translations::get('menu_fire')) . "</a><br><br>";
+	}
+
+	$ret .= "</li>
 </ol>
 ";
 
